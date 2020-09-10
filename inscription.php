bf4b05e68d1e506801dc14504a471a4253088ae4
Alexandre Vazquez <bvx67380@gmail.com>
2020-09-10 12:25:46 +0200

    update

--- inscription.php
+++ inscription.php
@@ -4,21 +4,15 @@ $dbh = new PDO('mysql:host=db;dbname=bdd', 'root', 'root');
 
 try {
     if (count($_POST) > 0) {
-        $sql = 'INSERT INTO employees (nom, prenom, email, `password`) VALUES (:nom, :prenom, :email, `password`)';
+        $sql = 'INSERT INTO employees (nom, prenom, email, `password`) VALUES (:nom, :prenom, :email, :`password`)';
 
         $stmt = $dbh->prepare($sql);
 
         $stmt->bindParam(":nom", $_POST['nom']);
         $stmt->bindParam(":prenom", $_POST['prenom']);
         $stmt->bindParam(":email", $_POST['email']);
-        $stmt->bindParam(":nom", $_POST['password']);
+        $stmt->bindParam(":password", $_POST['password']);
     }
       } catch (Exception $e) {
   echo $e->getMessage();
-
-foreach ($_POST as $key =>value) {
-  $stmt->bindParam(":".$key, $value);
- $stmt->bindParam(parameter:":".$key, &variable:$value);
-}
-
 }
